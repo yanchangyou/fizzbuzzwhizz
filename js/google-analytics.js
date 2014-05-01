1d1918dfe0f09554085ec16a0e74109524297419
yanchangyou <yanchangyou@gmail.com>
2014-05-01 18:04:17 +0800

    提交主界面

---
+++ js/google-analytics.js
@@ -0,0 +1,9 @@
+
+var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+
+document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+
+try {
+	var pageTracker = _gat._getTracker("UA-49239414-1");
+	pageTracker._trackPageview();
+} catch(err) {}
